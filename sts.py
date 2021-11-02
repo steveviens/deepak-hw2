0931814ad1cee95ac20aa7e1c63addca517eb2ca
Steve Viens <steve@viens.net>
2021-11-02 10:30:22 -0400

    first checked in

---
+++ sts.py
@@ -0,0 +1,19 @@
+#
+#  Copyright 2020-2021 Viens Consulting, LLC. All Rights Reserved.
+#
+
+import boto3
+
+# AWS Security Token Service API convienence class
+# https://docs.aws.amazon.com/STS/latest/APIReference/welcome.html
+
+
+class STS:
+
+    # Constructor: create an STS client
+    def __init__(self):
+        self.sts_client = boto3.client('sts')
+
+    # Retreive aws user identity info
+    def get_aws_identity(self):
+        return self.sts_client.get_caller_identity()
