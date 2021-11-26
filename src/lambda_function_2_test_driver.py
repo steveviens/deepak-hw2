47bbd507e604f515e623f665d0b4b8580955b75f
Steve Viens <steve@viens.net>
2021-11-26 11:12:25 -0500

    adding example cloudformation templates

---
+++ src/lambda_function_2_test_driver.py
@@ -0,0 +1,16 @@
+import json
+import logging
+import sys
+import lambda_function_2
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+
+
+event = {
+}
+
+results = lambda_function_2.lambda_handler(event, context={})
+print("HTTP STATUS  = {}".format(results.get('statusCode')))
+print('RESULTS={}'.format(json.dumps(results, indent=2, sort_keys=False)))
