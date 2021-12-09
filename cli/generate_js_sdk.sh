f5cdff998c4c16905f7d0d8e1018e46c9b1d7d46
Steve Viens <steve@viens.net>
2021-12-09 08:40:42 -0500

    integrate javascript sdk into build

--- cli/generate_js_sdk.sh
+++ cli/generate_js_sdk.sh
@@ -2,4 +2,4 @@ aws apigateway get-sdk \
             --rest-api-id d8n5fb21w7 \
             --stage-name v1 \
             --sdk-type javascript \
-            ./myApi-js-sdk.zip
+            ./hw2-js-sdk.zip
