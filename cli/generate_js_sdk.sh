cb78d0ac19e240eb97e8f8f6146e11b75c775e58
Steve Viens <steve@viens.net>
2021-12-09 13:59:19 -0500

    additional blending of open search cft

--- cli/generate_js_sdk.sh
+++
@@ -1,9 +0,0 @@
-aws apigateway get-sdk \
-            --rest-api-id d8n5fb21w7 \
-            --stage-name v1 \
-            --sdk-type javascript \
-            ./hw2-js-sdk.zip
-
-# REFERENCES:
-#   AWS API Gateway CLI Command Reference v2
-#   https://awscli.amazonaws.com/v2/documentation/api/latest/reference/apigateway/index.html
